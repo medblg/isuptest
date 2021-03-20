39be7600f540f4ceaa3edd5f06e25a9f86f9babe
s1m0x <s1m0x@yahoo.com>
2021-03-20 22:44:32 +0100

    timeout for live/offline hosts, no need to wait for offlines

--- isup.sh
+++ isup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-domain="$1"
-outts="$outt/$domain/subs"
 
-for sub in $(cat $outts/subs.txt) do
-    ping -c1 -W1 $sub > /dev/null 2>&1
-    if [[ $? -eq 0 ]]; # checking status code
-    then
-        echo -e "$sub" | tee -a $outts/alive.txt
+# usage: isup.sh domain.tld subs
+# adjusting to my rit func
 
-    else
-        echo -e "$foo" >>  $outts/Nalive.txt
-    fi
-done
+#check wildcard
+if [[ "$(dig @1.1.1.1 A,CNAME {test321123,testingforwildcard,plsdontgimmearesult,simooodfdfdf}.$1 +short | wc -l)" -gt "1" ]]; then
+    echo "[!] $1 -> Possible wildcard detected. check further"
+    exit
+else
+	#ping -c1 -W1 $1 > /dev/null 2>&1 && echo $1
+	#ping -c1 -w1 -n -4 $1 > /dev/null 2>&1 && echo $1
+	timeout 1 ping -c1 -n -4 $1 > /dev/null 2>&1 && echo $1
+fi
